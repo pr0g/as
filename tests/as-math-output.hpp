f51fe0b7d7c4ec8d720a3d807f301da0b118977d
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2019-06-25 22:12:46 +0100

    remove as-math-output from tests folder

--- tests/as-math-output.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-#include "as/as-vec.hpp"
-
-inline void print_vec2(const as::vec2_t& v) { printf("v = x: %f, y: %f\n", v.x, v.y); }
-inline void print_vec3(const as::vec3_t& v) { printf("v = x: %f, y: %f, z: %f\n", v.x, v.y, v.z); }
-inline void print_vec4(const as::vec4_t& v) { printf("v = x: %f, y: %f, z: %f, w: %f\n", v.x, v.y, v.z, v.w); }
