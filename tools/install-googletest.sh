ec4558d85f3d4b2f0f0e4bf441a36c4f8e3401e8
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2019-07-19 20:24:19 +0100

    attempt to get improved coverage numbers

--- tools/install-googletest.sh
+++ tools/install-googletest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 mkdir googletest && cd googletest
-git clone --depth=10 --branch=master https://github.com/google/googletest.git .
+git clone --depth=10 --branch=master https://github.com/pr0g/googletest.git .
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../tests/build/gtest_install/ ..
 cmake --build . --target install
