ad5f0235db24c5e06444ca75b448670d04d86e5e
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2019-08-02 21:24:55 +0100

    update windows build file to make it easier to run repeatedly

--- tools/misc/build-script.bat
+++ tools/misc/build-script.bat
@@ -1,12 +1,16 @@
 :: test for checking appveyor build_script locally (copy/move to root before running)
+@echo off
 
-mkdir catch2 && cd catch2
-git clone --depth=10 --branch=master https://github.com/catchorg/Catch2.git .
-mkdir build && cd build
+if not exist catch2 mkdir catch2
+cd catch2
+git clone --depth=1 --branch=master https://github.com/catchorg/Catch2.git .
+if not exist build mkdir build
+cd build
 cmake -G "Visual Studio 15 2017 Win64" -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=../../tests/build/catch2_install/ ..
 cmake --build . --config Release --target install
 cd ../..
-mkdir build && cd build
+if not exist build mkdir build
+cd build
 cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX=../tests/build/as_install/ ..
 cmake --build . --target install --config Release
 cd ../tests/build
