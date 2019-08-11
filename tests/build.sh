829d0d015b0f4f73f86423743ea4ef4c3aa7b381
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2019-08-11 20:28:32 +0100

    tidy up build.sh script

--- tests/build.sh
+++ tests/build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 [ ! -d "build" ] && mkdir -p "build"
-cd build/
-cmake -DDEV_LIB_DIR=../.. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=$(pwd)/as-install/ -DCMAKE_BUILD_TYPE=Release -DAS_PRECISION_FLOAT=ON -DAS_ROW_MAJOR=ON -DAS_COVERAGE=ON ..
-cmake --build .
-./as-test
-lcov --directory . --capture --output-file app.info --gcov-tool=/usr/local/bin/gcov-7
-lcov --remove app.info '*/catch2/*' '*/7.4.0/*' '*/usr/include/*' '*/usr/local/include/*' '*/tests/catch-*' '*/tests/test-*' -o app-filtered.info --gcov-tool=/usr/local/bin/gcov-7
-genhtml app-filtered.info --output-directory html/ --demangle-cpp
-open html/index.html
+cmake -B build/ -H.                                 \
+    -DDEV_LIB_DIR=..                                \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON              \
+    -DCMAKE_PREFIX_PATH=$(pwd)/build/as-install/    \
+    -DCMAKE_BUILD_TYPE=Release                      \
+    -DAS_PRECISION_FLOAT=ON                         \
+    -DAS_ROW_MAJOR=ON                               \
+    -DAS_COVERAGE=ON
+cmake --build build/
+./build/as-test
+lcov --directory build/ --capture   \
+    --output-file build/app.info    \
+    --gcov-tool=/usr/local/bin/gcov-7
+lcov --remove build/app.info                                    \
+    '*/catch2/*' '*/7.4.0/*' '*/usr/include/*'                  \
+    '*/usr/local/include/*' '*/tests/catch-*' '*/tests/test-*'  \
+    -o build/app-filtered.info --gcov-tool=/usr/local/bin/gcov-7
+genhtml build/app-filtered.info --output-directory build/html/ --demangle-cpp
+open build/html/index.html
