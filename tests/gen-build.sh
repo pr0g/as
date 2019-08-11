09dab00cd56b7a83ccb60a38afa8e005e5039573
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2019-08-11 18:53:55 +0100

    small fix to gen-build.sh script

--- tests/gen-build.sh
+++ tests/gen-build.sh
@@ -11,11 +11,11 @@
 # DCMAKE_PREFIX_PATH
 
 [ ! -d "build" ] && mkdir -p "build"
-cmake -B build/ -H. -DDEV_LIB_DIR=..        \
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON      \
-    -DCMAKE_PREFIX_PATH=$(pwd)/as-install/  \
-    -DCMAKE_BUILD_TYPE=Release              \
-    -DAS_PRECISION_FLOAT=ON                 \
-    -DAS_COL_MAJOR=ON                       \
+cmake -B build/ -H. -DDEV_LIB_DIR=..                \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON              \
+    -DCMAKE_PREFIX_PATH=$(pwd)/build/as-install/    \
+    -DCMAKE_BUILD_TYPE=Release                      \
+    -DAS_PRECISION_FLOAT=ON                         \
+    -DAS_COL_MAJOR=ON                               \
     -DAS_COVERAGE=ON
 cmake --build build/
