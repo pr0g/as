58343df8c6ad51e1fbfa3f219a071242f74d982a
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2019-08-11 18:43:04 +0100

    add build script reminder for tests

---
+++ tests/gen-build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# options
+# DAS_PRECISION_FLOAT || DAS_PRECISION_DOUBLE
+# DAS_COL_MAJOR || DAS_ROW_MAJOR
+# DAS_COVERAGE
+# DCMAKE_BUILD_TYPE=Release || DCMAKE_BUILD_TYPE=Debug
+
+# build
+# DCMAKE_EXPORT_COMPILE_COMMANDS
+# DCMAKE_PREFIX_PATH
+
+[ ! -d "build" ] && mkdir -p "build"
+cmake -B build/ -H. -DDEV_LIB_DIR=..        \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON      \
+    -DCMAKE_PREFIX_PATH=$(pwd)/as-install/  \
+    -DCMAKE_BUILD_TYPE=Release              \
+    -DAS_PRECISION_FLOAT=ON                 \
+    -DAS_COL_MAJOR=ON                       \
+    -DAS_COVERAGE=ON
+cmake --build build/
