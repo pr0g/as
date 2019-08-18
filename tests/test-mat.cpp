61157336cfd38a2ce86593c6459afb1ec98b8eb7
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2019-08-18 20:49:16 +0100

    add another axis angle assertion

--- tests/test-mat.cpp
+++ tests/test-mat.cpp
@@ -958,21 +958,43 @@ TEST_CASE("mat3_axis_angle", "[as-mat]")
 {
     using namespace gsl;
 
-    as::mat3_t axis_angle = as::mat3::axis_angle(as::vec3::axis_x(), as::deg_to_rad(90.0f));
-    as::real_t result_reference[] = { 0.0f, -1.0f, 0.0f };
+    {
+        as::mat3_t axis_angle =
+            as::mat3::axis_angle(as::vec3::axis_x(), as::deg_to_rad(90.0f));
+        as::real_t result_reference[] = { 0.0f, -1.0f, 0.0f };
 
-    as::vec3_t result;
+        as::vec3_t result;
 #ifdef AS_ROW_MAJOR
-    result = as::vec3::axis_z() * axis_angle;
+        result = as::vec3::axis_z() * axis_angle;
 #elif defined AS_COL_MAJOR
-    result = axis_angle * as::vec3::axis_z();
+        result = axis_angle * as::vec3::axis_z();
 #endif // AS_ROW_MAJOR ? AS_COL_MAJOR
 
-    // note - checking against 0.0f requires margin
-    CHECK_THAT(
-        span(result_reference),
-        ElementsAreSubscript(result, 3).margin(std::numeric_limits<float>::epsilon())
-    );
+        // note - checking against 0.0f requires margin
+        CHECK_THAT(
+            span(result_reference),
+            ElementsAreSubscript(result, 3).margin(std::numeric_limits<float>::epsilon())
+        );
+    }
+
+    {
+        as::mat3_t axis_angle =
+            as::mat3::axis_angle(as::vec3::axis_y(), as::deg_to_rad(90.0f));
+        as::real_t result_reference[] = { 0.0f, 0.0f, -1.0f };
+
+        as::vec3_t result;
+#ifdef AS_ROW_MAJOR
+        result = as::vec3::axis_x() * axis_angle;
+#elif defined AS_COL_MAJOR
+        result = axis_angle * as::vec3::axis_x();
+#endif // AS_ROW_MAJOR ? AS_COL_MAJOR
+
+        // note - checking against 0.0f requires margin
+        CHECK_THAT(
+            span(result_reference),
+            ElementsAreSubscript(result, 3).margin(std::numeric_limits<float>::epsilon())
+        );
+    }
 }
 
 // explicit instantiations (for coverage)
