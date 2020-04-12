6ad66b7a9a8d4097e271587848d93959f08b8b70
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-04-12 17:38:23 +0100

    run clang-format on remaining files

--- tests/bench/as-vec.bench.cpp
+++ tests/bench/as-vec.bench.cpp
@@ -8,6 +8,7 @@ TEST_CASE("as-vec-dot", "[as-vec]")
 {
     BENCHMARK("as-vec")
     {
-        return as::vec::dot(as::vec3_t{1.0f, 2.0f, 3.0f}, as::vec3_t{5.0f, 6.0f, 7.0f});
+        return as::vec::dot(
+            as::vec3_t{1.0f, 2.0f, 3.0f}, as::vec3_t{5.0f, 6.0f, 7.0f});
     };
 }
