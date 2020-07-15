9eedc4806372cb8d70a5bdfffbaf8adee223f91f
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-07-15 20:33:29 +0100

    add some updated benchmarks

---
+++ tests/bench/as-affine.bench.cpp
@@ -0,0 +1,17 @@
+#include "catch2/catch.hpp"
+
+#include "as/as-math-ops.hpp"
+
+using as::operator""_r;
+
+TEST_CASE("as-affine", "[as_affine]")
+{
+    BENCHMARK_ADVANCED("as-affine-inverse")(Catch::Benchmark::Chronometer meter)
+    {
+        as::affine_t affine = as::affine_t{
+            as::mat3::rotation_x(as::deg_to_rad(45.0_r)),
+            as::point3_t{1.0_r, 2.0_r, 3.0_r}};
+
+        meter.measure([&affine] { return as::affine::inverse(affine); });
+    };
+}
--- tests/bench/as-vec.bench.cpp
+++ tests/bench/as-vec.bench.cpp
@@ -2,11 +2,11 @@
 
 #include "as/as-math-ops.hpp"
 
-using namespace as;
+using as::operator""_r;
 
-TEST_CASE("as-vec-dot", "[as_vec]")
+TEST_CASE("as-vec", "[as_vec]")
 {
-    BENCHMARK("as-vec")
+    BENCHMARK("as-vec-dot")
     {
         return as::vec::dot(
             as::vec3_t{1.0_r, 2.0_r, 3.0_r}, as::vec3_t{5.0_r, 6.0_r, 7.0_r});
