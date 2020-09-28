9c6238d55cfcefe6b70ffac5216ce638d6bfb29f
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-09-28 20:41:13 +0100

    shift as-point to be a templated type

--- include/as/as-point.hpp
+++ include/as/as-point.hpp
@@ -9,145 +9,176 @@
 namespace as
 {
 
-//! Represents a two dimensional p.
-//! A p provides many of the same operations as a vector but has a more
+//! A geometric point class template parameterized by type and dimension.
+template<typename T, index d>
+struct point
+{
+  // unused
+};
+
+//! Represents a two dimensional point.
+//! A point provides many of the same operations as a vector but has a more
 //! restrictive interface. It is geometrically very different from a vector and
 //! can be used to increase type safety and prevent erroneous operations.
-struct point2
+template<typename T>
+struct point<T, 2>
 {
-  //! Type alias for ::real.
-  using value_type = real;
+  //! Type alias for template parameter `T`.
+  using value_type = T;
 
-  point2() noexcept = default;
+  point<T, 2>() noexcept = default;
 
   //! Constructs point2 with `(xy_, xy_)`.
-  constexpr explicit point2(real xy_);
+  constexpr explicit point<T, 2>(T xy_);
   //! Constructs point2 with `(xy_.x, xy_.y)`.
-  constexpr explicit point2(const vec2& xy_);
+  constexpr explicit point<T, 2>(const vec<T, 2>& xy_);
   //! Constructs point2 with `(x_, y_)`.
-  constexpr point2(real x_, real y_);
+  constexpr point<T, 2>(T x_, T y_);
 
   //! Returns a mutable reference to the value at the given index.
   //! \note Can only be called on a mutable lvalue object.
   //! \warning No bounds checking is performed.
-  real& operator[](index i) &;
+  T& operator[](index i) &;
   //! Returns a const reference to the value at the given index.
   //! \note Can only be called on a const lvalue object.
   //! \warning No bounds checking is performed.
-  const real& operator[](index i) const&;
+  const T& operator[](index i) const&;
   //! Returns a copy of the value at the given index.
   //! \note Can only be called on an rvalue object.
   //! \warning No bounds checking is performed.
-  real operator[](index i) &&;
+  T operator[](index i) &&;
 
   //! Returns `2`.
   constexpr static index size();
   //! Returns `(0, 0)`.
-  constexpr static point2 zero();
+  constexpr static point<T, 2> zero();
   //! Returns `(x, 0)`.
   //! \param x Optional parameter for the length of the axis, defaults to 1.
-  constexpr static point2 axis_x(real x = 1.0_r);
+  constexpr static point<T, 2> axis_x(T x = T(1.0));
   //! Returns `(0, y)`.
   //! \param y Optional parameter for the length of the axis, defaults to 1.
-  constexpr static point2 axis_y(real y = 1.0_r);
+  constexpr static point<T, 2> axis_y(T y = T(1.0));
 
   //! Returns a copy of the underlying vec2.
   //! \note Can only be called on an rvalue object.
-  constexpr vec2 as_vec() &&;
+  constexpr vec<T, 2> as_vec() &&;
   //! Returns a const reference to the underlying vec2.
   //! \note Can only be called on a const lvalue object.
-  [[nodiscard]] constexpr const vec2& as_vec() const&;
+  [[nodiscard]] constexpr const vec<T, 2>& as_vec() const&;
 
 private:
   //! Returns the result of the right hand point2 subtracted from the left
   //! hand point2.
-  friend constexpr const vec2 operator-(const point2& lhs, const point2& rhs);
+  friend constexpr const vec<T, 2> operator-(const point<T, 2>& lhs, const point<T, 2>& rhs);
   //! Returns the result of a vec2 added to a point2.
-  friend constexpr const point2 operator+(const point2& p, const vec2& v);
+  friend constexpr const point<T, 2> operator+(const point<T, 2>& p, const vec<T, 2>& v);
   //! Performs addition assignment of a point2 and a vec2.
-  friend constexpr point2& operator+=(point2& p, const vec2& v);
+  friend constexpr point<T, 2>& operator+=(point<T, 2>& p, const vec<T, 2>& v);
   //! Returns the result of a vec2 subtracted from a point2.
-  friend constexpr const point2 operator-(const point2& p, const vec2& v);
+  friend constexpr const point<T, 2> operator-(const point<T, 2>& p, const vec<T, 2>& v);
   //! Performs subtraction assignment of a point2 and a vec2.
-  friend constexpr point2& operator-=(point2& p, const vec2& v);
+  friend constexpr point<T, 2>& operator-=(point<T, 2>& p, const vec<T, 2>& v);
   //! Returns the negation of a point2.
-  friend const point2 operator-(const point2& p);
+  friend constexpr point<T, 2> operator-(const point<T, 2>& p);
 
-  vec2 v; //!< point2 is implemented in terms of vec2.
+  vec<T, 2> v; //!< point2 is implemented in terms of vec2.
 };
 
-//! Represents a three dimensional p.
-//! A p provides many of the same operations as a vector but has a more
+//! Type alias for a two dimensional point of type ::real.
+using point2 = point<real, 2>;
+//! Type alias for a two dimensional point of type `float`.
+using point2f = point<float, 2>;
+//! Type alias for a two dimensional point of type `double`.
+using point2d = point<double, 2>;
+//! Type alias for a two dimensional point of type `int32_t`.
+using point2i = point<int32_t, 2>;
+//! Type alias for a two dimensional point of type `int64_t`.
+using point2l = point<int64_t, 2>;
+
+//! Represents a three dimensional point.
+//! A point provides many of the same operations as a vector but has a more
 //! restrictive interface. It is geometrically very different from a vector and
 //! can be used to increase type safety and prevent erroneous operations.
-struct point3
+template<typename T>
+struct point<T, 3>
 {
-  //! Type alias for ::real.
-  using value_type = real;
+  //! Type alias for template parameter `T`.
+  using value_type = T;
 
-  point3() noexcept = default;
+  point<T, 3>() noexcept = default;
 
   //! Constructs point3 with `(xyz_, xyz_, xyz_)`.
-  constexpr explicit point3(real xyz_);
+  constexpr explicit point<T, 3>(T xyz_);
   //! Constructs point3 with `(v_.x, v_.y, v_.z)`.
-  constexpr explicit point3(const vec3& v_);
+  constexpr explicit point<T, 3>(const vec<T, 3>& v_);
   //! Constructs point3 with `(x_, y_, z_)`.
-  constexpr point3(real x_, real y_, real z_);
+  constexpr point<T, 3>(T x_, T y_, T z_);
   //! Constructs point3 with `(xy_.x, xy_.y, z_)`.
-  constexpr point3(const vec2& xy_, real z_);
+  constexpr point<T, 3>(const vec<T, 2>& xy_, T z_);
 
   //! Returns a mutable reference to the value at the given index.
   //! \note Can only be called on a mutable lvalue object.
   //! \warning No bounds checking is performed.
-  real& operator[](index i) &;
+  T& operator[](index i) &;
   //! Returns a const reference to the value at the given index.
   //! \note Can only be called on a const lvalue object.
   //! \warning No bounds checking is performed.
-  const real& operator[](index i) const&;
+  const T& operator[](index i) const&;
   //! Returns a copy of the value at the given index.
   //! \note Can only be called on an rvalue object.
   //! \warning No bounds checking is performed.
-  real operator[](index i) &&;
+  T operator[](index i) &&;
 
   //! Returns `3`.
   constexpr static index size();
 
   //! Returns `(0, 0, 0)`.
-  constexpr static point3 zero();
+  constexpr static point<T, 3> zero();
   //! Returns `(x, 0, 0)`.
   //! \param x Optional parameter for the length of the axis, defaults to 1.
-  constexpr static point3 axis_x(real x = 1.0_r);
+  constexpr static point<T, 3> axis_x(T x = T(1.0));
   //! Returns `(0, y, 0)`.
   //! \param y Optional parameter for the length of the axis, defaults to 1.
-  constexpr static point3 axis_y(real y = 1.0_r);
+  constexpr static point<T, 3> axis_y(T y = T(1.0));
   //! Returns `(0, 0, z)`.
   //! \param z Optional parameter for the length of the axis, defaults to 1.
-  constexpr static point3 axis_z(real z = 1.0_r);
+  constexpr static point<T, 3> axis_z(T z = T(1.0));
 
   //! Returns a copy of the underlying vec3.
   //! \note Can only be called on an rvalue object.
-  constexpr vec3 as_vec() &&;
+  constexpr vec<T, 3> as_vec() &&;
   //! Returns a const reference to the underlying vec3.
   //! \note Can only be called on a const lvalue object.
-  [[nodiscard]] constexpr const vec3& as_vec() const&;
+  [[nodiscard]] constexpr const vec<T, 3>& as_vec() const&;
 
 private:
   //! Returns the result of the right hand point3 subtracted from the left
   //! hand point3.
-  friend constexpr const vec3 operator-(const point3& lhs, const point3& rhs);
+  friend constexpr const vec<T, 3> operator-(const point<T, 3>& lhs, const point<T, 3>& rhs);
   //! Returns the result of a vec3 added to a point3.
-  friend constexpr const point3 operator+(const point3& p, const vec3& v);
+  friend constexpr const point<T, 3> operator+(const point<T, 3>& p, const vec<T, 3>& v);
   //! Performs addition assignment of a point3 and a vec3.
-  friend constexpr point3& operator+=(point3& p, const vec3& v);
+  friend constexpr point<T, 3>& operator+=(point<T, 3>& p, const vec<T, 3>& v);
   //! Returns the result of a vec3 subtracted from a point3.
-  friend constexpr const point3 operator-(const point3& p, const vec3& v);
+  friend constexpr const point<T, 3> operator-(const point<T, 3>& p, const vec<T, 3>& v);
   //! Performs subtraction assignment of a point3 and a vec3.
-  friend constexpr point3& operator-=(point3& p, const vec3& v);
+  friend constexpr point<T, 3>& operator-=(point<T, 3>& p, const vec<T, 3>& v);
   //! Returns the negation of a point3.
-  friend constexpr const point3 operator-(const point3& p);
+  friend constexpr const point<T, 3> operator-(const point<T, 3>& p);
 
-  vec3 v; //!< point3 is implemented in terms of vec3.
+  vec<T, 3> v; //!< point3 is implemented in terms of vec3.
 };
+
+//! Type alias for a three dimensional point of type ::real.
+using point3 = point<real, 3>;
+//! Type alias for a three dimensional point of type `float`.
+using point3f = point<float, 3>;
+//! Type alias for a three dimensional point of type `double`.
+using point3d = point<double, 3>;
+//! Type alias for a three dimensional point of type `int32_t`.
+using point3i = point<int32_t, 3>;
+//! Type alias for a three dimensional point of type `int64_t`.
+using point3l = point<int64_t, 3>;
 
 } // namespace as
 
--- include/as/as-point.inl
+++ include/as/as-point.inl
@@ -1,193 +1,231 @@
 namespace as
 {
 
-AS_API constexpr point2::point2(const real xy_) : v{xy_, xy_}
+template<typename T>
+AS_API constexpr point<T, 2>::point(const T xy_) : v{xy_, xy_}
 {
 }
 
-AS_API constexpr point2::point2(const vec2& xy_) : v{xy_}
+template<typename T>
+AS_API constexpr point<T, 2>::point(const vec<T, 2>& xy_) : v{xy_}
 {
 }
 
-AS_API constexpr point2::point2(const real x_, const real y_) : v{x_, y_}
+template<typename T>
+AS_API constexpr point<T, 2>::point(const T x_, const T y_) : v{x_, y_}
 {
 }
 
-AS_API inline real& point2::operator[](const index i) &
-{
-  return v[i];
-}
-
-AS_API inline const real& point2::operator[](const index i) const&
-{
-  return v[i];
-}
-
-AS_API inline real point2::operator[](const index i) &&
+template<typename T>
+AS_API inline T& point<T, 2>::operator[](const index i) &
 {
   return v[i];
 }
 
-AS_API constexpr index point2::size()
+template<typename T>
+AS_API inline const T& point<T, 2>::operator[](const index i) const&
+{
+  return v[i];
+}
+
+template<typename T>
+AS_API inline T point<T, 2>::operator[](const index i) &&
+{
+  return v[i];
+}
+
+template<typename T>
+AS_API constexpr index point<T, 2>::size()
 {
   return 2;
 }
 
-AS_API constexpr point2 point2::zero()
+template<typename T>
+AS_API constexpr point<T, 2> point<T, 2>::zero()
 {
-  return point2{vec2::zero()};
+  return point<T, 2>{vec<T, 2>::zero()};
 }
 
-AS_API constexpr point2 point2::axis_x(const real x /* = 1.0_r */)
+template<typename T>
+AS_API constexpr point<T, 2> point<T, 2>::axis_x(const T x /* = 1.0_r */)
 {
-  return point2{vec2::axis_x(x)};
+  return point<T, 2>{vec<T, 2>::axis_x(x)};
 }
 
-AS_API constexpr point2 point2::axis_y(const real y /* = 1.0_r */)
+template<typename T>
+AS_API constexpr point<T, 2> point<T, 2>::axis_y(const T y /* = 1.0_r */)
 {
-  return point2{vec2::axis_y(y)};
+  return point<T, 2>{vec<T, 2>::axis_y(y)};
 }
 
-AS_API constexpr vec2 point2::as_vec() &&
+template<typename T>
+AS_API constexpr vec<T, 2> point<T, 2>::as_vec() &&
 {
   return v;
 }
 
-AS_API constexpr const vec2& point2::as_vec() const&
+template<typename T>
+AS_API constexpr const vec<T, 2>& point<T, 2>::as_vec() const&
 {
   return v;
 }
 
-AS_API constexpr const vec2 operator-(const point2& lhs, const point2& rhs)
+template<typename T>
+AS_API constexpr const vec<T, 2> operator-(const point<T, 2>& lhs, const point<T, 2>& rhs)
 {
   return lhs.v - rhs.v;
 }
 
-AS_API constexpr const point2 operator+(const point2& p, const vec2& v)
+template<typename T>
+AS_API constexpr const point<T, 2> operator+(const point<T, 2>& p, const vec<T, 2>& v)
 {
-  return point2(p.v + v);
+  return point<T, 2>(p.v + v);
 }
 
-AS_API constexpr point2& operator+=(point2& p, const vec2& v)
+template<typename T>
+AS_API constexpr point<T, 2>& operator+=(point<T, 2>& p, const vec<T, 2>& v)
 {
   p.v += v;
   return p;
 }
 
-AS_API constexpr const point2 operator-(const point2& p, const vec2& v)
+template<typename T>
+AS_API constexpr const point<T, 2> operator-(const point<T, 2>& p, const vec<T, 2>& v)
 {
-  return point2(p.v - v);
+  return point<T, 2>(p.v - v);
 }
 
-AS_API constexpr point2& operator-=(point2& p, const vec2& v)
+template<typename T>
+AS_API constexpr point<T, 2>& operator-=(point<T, 2>& p, const vec<T, 2>& v)
 {
   p.v -= v;
   return p;
 }
 
-AS_API inline const point2 operator-(const point2& p)
+template<typename T>
+AS_API constexpr const point<T, 2> operator-(const point<T, 2>& p)
 {
-  return point2{-p.v};
+  return point<T, 2>{-p.v};
 }
 
-AS_API constexpr point3::point3(const real xyz_) : v{xyz_, xyz_, xyz_}
-{
-}
-
-AS_API constexpr point3::point3(const vec3& v_) : v{v_}
+template<typename T>
+AS_API constexpr point<T, 3>::point(const T xyz_) : v{xyz_, xyz_, xyz_}
 {
 }
 
-AS_API constexpr point3::point3(const real x_, const real y_, const real z_)
+template<typename T>
+AS_API constexpr point<T, 3>::point(const vec<T, 3>& v_) : v{v_}
+{
+}
+
+template<typename T>
+AS_API constexpr point<T, 3>::point(const T x_, const T y_, const T z_)
   : v{x_, y_, z_}
 {
 }
 
-AS_API constexpr point3::point3(const vec2& xy_, const real z_)
+template<typename T>
+AS_API constexpr point<T, 3>::point(const vec<T, 2>& xy_, const T z_)
   : v{xy_.x, xy_.y, z_}
 {
 }
 
-AS_API inline real& point3::operator[](const index i) &
+template<typename T>
+AS_API inline T& point<T, 3>::operator[](const index i) &
 {
   return v[i];
 }
 
-AS_API inline const real& point3::operator[](const index i) const&
+template<typename T>
+AS_API inline const T& point<T, 3>::operator[](const index i) const&
 {
   return v[i];
 }
 
-AS_API inline real point3::operator[](const index i) &&
+template<typename T>
+AS_API inline T point<T, 3>::operator[](const index i) &&
 {
   return v[i];
 }
 
-AS_API constexpr index point3::size()
+template<typename T>
+AS_API constexpr index point<T, 3>::size()
 {
   return 3;
 }
 
-AS_API constexpr point3 point3::zero()
+template<typename T>
+AS_API constexpr point<T, 3> point<T, 3>::zero()
 {
-  return point3{vec3::zero()};
+  return point<T, 3>{vec<T, 3>::zero()};
 }
 
-AS_API constexpr point3 point3::axis_x(const real x /* = 1.0_r */)
+template<typename T>
+AS_API constexpr point<T, 3> point<T, 3>::axis_x(const T x /* = 1.0_r */)
 {
-  return point3{vec3::axis_x(x)};
+  return point<T, 3>{vec<T, 3>::axis_x(x)};
 }
 
-AS_API constexpr point3 point3::axis_y(const real y /* = 1.0_r */)
+template<typename T>
+AS_API constexpr point<T, 3> point<T, 3>::axis_y(const T y /* = 1.0_r */)
 {
-  return point3{vec3::axis_y(y)};
+  return point<T, 3>{vec<T, 3>::axis_y(y)};
 }
 
-AS_API constexpr point3 point3::axis_z(const real z /* = 1.0_r */)
+template<typename T>
+AS_API constexpr point<T, 3> point<T, 3>::axis_z(const T z /* = 1.0_r */)
 {
-  return point3{vec3::axis_z(z)};
+  return point<T, 3>{vec<T, 3>::axis_z(z)};
 }
 
-AS_API constexpr vec3 point3::as_vec() &&
+template<typename T>
+AS_API constexpr vec<T, 3> point<T, 3>::as_vec() &&
 {
   return v;
 }
 
-AS_API constexpr const vec3& point3::as_vec() const&
+template<typename T>
+AS_API constexpr const vec<T, 3>& point<T, 3>::as_vec() const&
 {
   return v;
 }
 
-AS_API constexpr const vec3 operator-(const point3& lhs, const point3& rhs)
+template<typename T>
+AS_API constexpr const vec<T, 3> operator-(const point<T, 3>& lhs, const point<T, 3>& rhs)
 {
   return lhs.v - rhs.v;
 }
 
-AS_API constexpr const point3 operator+(const point3& p, const vec3& v)
+template<typename T>
+AS_API constexpr const point<T, 3> operator+(const point<T, 3>& p, const vec3& v)
 {
-  return point3(p.v + v);
+  return point<T, 3>(p.v + v);
 }
 
-AS_API constexpr point3& operator+=(point3& p, const vec3& v)
+template<typename T>
+AS_API constexpr point<T, 3>& operator+=(point<T, 3>& p, const vec3& v)
 {
   p.v += v;
   return p;
 }
 
-AS_API constexpr const point3 operator-(const point3& p, const vec3& v)
+template<typename T>
+AS_API constexpr const point<T, 3> operator-(const point<T, 3>& p, const vec3& v)
 {
-  return point3(p.v - v);
+  return point<T, 3>(p.v - v);
 }
 
-AS_API constexpr point3& operator-=(point3& p, const vec3& v)
+template<typename T>
+AS_API constexpr point<T, 3>& operator-=(point<T, 3>& p, const vec3& v)
 {
   p.v -= v;
   return p;
 }
 
-AS_API constexpr const point3 operator-(const point3& p)
+template<typename T>
+AS_API constexpr const point<T, 3> operator-(const point<T, 3>& p)
 {
-  return point3{-p.v};
+  return point<T, 3>{-p.v};
 }
 
 } // namespace as
