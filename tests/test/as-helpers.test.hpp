8b0f52dfe11f830f8adb9174de0ff78aa4c5ec82
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-01-02 21:38:18 +0000

    add braces around initialization of subobject

--- tests/test/as-helpers.test.hpp
+++ tests/test/as-helpers.test.hpp
@@ -12,19 +12,19 @@ namespace unit_test
 template<typename T>
 std::array<T, 2> arr(const T x, const T y)
 {
-  return std::array<T, 2>{x, y};
+  return std::array<T, 2>{{x, y}};
 }
 
 template<typename T>
 std::array<T, 3> arr(const T x, const T y, const T z)
 {
-  return std::array<T, 3>{x, y, z};
+  return std::array<T, 3>{{x, y, z}};
 }
 
 template<typename T>
 std::array<T, 4> arr(const T x, const T y, const T z, const T w)
 {
-  return std::array<T, 4>{x, y, z, w};
+  return std::array<T, 4>{{x, y, z, w}};
 }
 
 template<typename T, as::index d>
