dc68427a4fbf486e15d293bef7cd4efa269a6161
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-01-02 18:24:36 +0000

    remove demangle option to to local failure - c++filt version mismatch

--- tests/test/do-coverage.sh
+++ tests/test/do-coverage.sh
@@ -9,5 +9,9 @@ lcov --remove build/app.info                                    \
     '*/catch2/*' '*/7.4.0/*' '*/usr/include/*'                  \
     '*/usr/local/include/*' '*/tests/test/catch-*' '*/tests/test/*.test.*'  \
     -o build/app-filtered.info --gcov-tool=/usr/local/bin/gcov-7
-genhtml build/app-filtered.info --output-directory build/html/ --demangle-cpp
+genhtml build/app-filtered.info --output-directory build/html/
 open build/html/index.html
+
+# note: removed --demangle-cpp from genhtml as c++filt that is called
+# internally accepts '--no-strip-underscore' instead of '--no-strip-underscores',
+# likely caused by version mismatch due to os/compiler update
