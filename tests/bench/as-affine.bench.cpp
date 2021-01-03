6fb2965dc3d1ebd7a2ab52559851da42f9cbee2d
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-01-03 19:52:29 +0000

    run clang-format

--- tests/bench/as-affine.bench.cpp
+++ tests/bench/as-affine.bench.cpp
@@ -9,8 +9,7 @@ TEST_CASE("as-affine", "[as_affine]")
   BENCHMARK_ADVANCED("as-affine-inverse")(Catch::Benchmark::Chronometer meter)
   {
     as::affine a = as::affine{
-      as::mat3_rotation_x(as::radians(45.0_r)),
-      as::vec3(1.0_r, 2.0_r, 3.0_r)};
+      as::mat3_rotation_x(as::radians(45.0_r)), as::vec3(1.0_r, 2.0_r, 3.0_r)};
 
     meter.measure([&a] { return as::affine_inverse(a); });
   };
