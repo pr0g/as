3fd7b10970e7d73e1a2194a192e670b228b27a7e
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-01-04 21:20:34 +0000

    update build for GSL due to breaking change addition of GSL_INSTALL CMake option

--- tools/install-gsl.sh
+++ tools/install-gsl.sh
@@ -2,5 +2,5 @@
 set -ev
 mkdir gsl && cd gsl
 git clone --depth=1 https://github.com/microsoft/GSL.git .
-cmake -S . -B build -DGSL_TEST=OFF -DCMAKE_INSTALL_PREFIX=../tests/test/build/gsl_install/
+cmake -S . -B build -DGSL_TEST=OFF -DGSL_INSTALL=ON -DCMAKE_INSTALL_PREFIX=../tests/test/build/gsl_install/
 cmake --build build --target install
