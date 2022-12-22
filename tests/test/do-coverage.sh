249c3ac2461f48ede9dba889d07aadcffc29c058
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-12-22 10:52:13 +0000

    update local coverage script

--- tests/test/do-coverage.sh
+++ tests/test/do-coverage.sh
@@ -4,13 +4,13 @@
 ./build/as-test
 lcov --directory build/ --capture                                 \
      --output-file build/app.info                                 \
-     --gcov-tool=/usr/local/bin/gcov-10
+     --gcov-tool=/opt/homebrew/bin/gcov-12
 lcov --remove build/app.info                                      \
-    '*/10.3.0/*' '*/usr/include/*'                                 \
+    '*/12.1.0/*' '*/usr/include/*'                                 \
     '*/usr/local/include/*' '*/tests/test/catch-*'                \
     '*/tests/test/*.test.*' '*/third-party/*'                     \
     -o build/app-filtered.info                                    \
-    --gcov-tool=/usr/local/bin/gcov-10
+    --gcov-tool=/opt/homebrew/bin/gcov-12
 genhtml build/app-filtered.info --output-directory build/html/
 open build/html/index.html
 
